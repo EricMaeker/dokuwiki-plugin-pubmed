111daf50097f29655a9354a0b55473a4260f1a05
Eric Maeker <eric.maeker@gmail.com>
2017-05-14 12:19:28 +0200

    Correcting error message

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,6 +1,6 @@
 <?php
   $lang['pubmed_not_found']     = 'PMID: %s was not found.';
   $lang['pubmed_wrong_format']  = 'Syntax error [pubmed plugin]';
-  $lang['pubmed_available_cmd'] ='[Available commands]: short, long, long_abstract, summaryXML, clear_summary, remove_dir';
+  $lang['pubmed_available_cmd'] ='[Available commands]: short, long, long_abstract, summaryXML, clear_summary, remove_dir, search';
   $lang['plugin_cmd_not_found'] = "Commands '%s' was not found.";
 ?>
