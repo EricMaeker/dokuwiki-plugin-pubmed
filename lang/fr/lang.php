7ba948b0c7e57388682c7c3db7138734e9e3fa6f
Eric Maeker <eric.maeker@gmail.com>
2017-05-14 11:35:21 +0200

    Correcting error message

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -1,6 +1,6 @@
 <?php
   $lang['pubmed_not_found']     = 'PMID: %s introuvable.';
   $lang['pubmed_wrong_format']  = 'Erreur de syntaxe [greffon pubmed]';
-  $lang['pubmed_available_cmd'] = '[Commandes disponibles]: long, short, long_abstract, summaryXML, clear_summary, remove_dir, search';
+  $lang['pubmed_available_cmd'] = '[Commandes disponibles]: short, long, long_abstract, summaryXML, clear_summary, remove_dir, search';
   $lang['plugin_cmd_not_found'] = "Commande '%s' introuvable.";
 ?>
